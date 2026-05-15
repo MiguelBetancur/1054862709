23bb733acaa92c23116d0be72dc50ff4970e0671
mibetancurv <mibetancurv@unal.edu.co>
2026-05-15 15:42:51 -0500

    Últimmo Commit

--- public/consumirApi.js
+++ public/consumirApi.js
@@ -1,13 +1,13 @@
 function consumirApi(){
     fetch('endPoint')
-    .then(response => response.json())
-    .then(cantidades => {
-        let mayor = cantidades[0];
+        .then(response => response.json())
+        .then(cantidades => {
+            let mayor = cantidades[0];
 
-        for (let i=1;i<cantidades.length;i++){
-            if(cantidades[i]>mayor){
-                mayor=cantidades[i];
+            for (let i=1;i<cantidades.length;i++){
+                if(cantidades[i]>mayor){
+                    mayor=cantidades[i];
+                }
             }
-        }
-    })
+        })
 }
